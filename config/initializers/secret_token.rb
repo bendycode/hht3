a87ec6053fbaeca8047f77489b67c5a40d5fb4de
bendycode <stephen@bendyworks.com>
2012-10-20 14:06:57 -0500

    Initial, empty Rails 3 project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hht3::Application.config.secret_token = 'e3db1c464ccac8e1c15b41993ada19e43e80259862cf47696f08e14a1075af1d17ad5b3831f99c1e1ce7d97c7894302cc22709f30a6831a8e6ee2875d1a90b8b'
